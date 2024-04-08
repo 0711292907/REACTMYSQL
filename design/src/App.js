1cc3177adf28fdec2e5f78f67852aa0b3f60b849
Jason <murangira99@gmail.com>
2024-04-08 13:14:08 +0200

    added react router dom

--- design/src/App.js
+++ design/src/App.js
@@ -1,8 +1,12 @@
  import React from "react";
- 
+ import {BrowserRouter, Routes ,Route} from 'react-router-dom';
  function App() {
   return (
-   <div>App</div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element ={<Home/>}/>
+   </Routes>
+   </BrowserRouter>
   )
 } 
 

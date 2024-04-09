be1b65e0f993493b3a2f41db643230a1e7288a6c
Jason <murangira99@gmail.com>
2024-04-09 09:32:21 +0200

    added use effect and axios

--- design/src/Home.jsx
+++ design/src/Home.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React,{ useEffect  } from 'react'
+import axios from 'ax'
 
 function Home (){
+
+    useEffect(()=>{
+        axios.get ('http://localhost:8081/')
+        .then(res => console.log(res))
+        .catch (err=>console.log(err));
+    }, {})
     return (
-        <div>
-            Home
-        </div>
+        <>   
+        </>
     )
     }
     export default Home

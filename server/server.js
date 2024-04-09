bb57eabea1148e29c034dc7c60e4b4fe94563054
Jason <murangira99@gmail.com>
2024-04-09 10:08:32 +0200

    added a divs

--- server/server.js
+++ server/server.js
@@ -7,6 +7,31 @@ const app = express();
 
 app.use(cors());
 
+
+// Create a MySQL connection 
+const db= mysql.createPool({
+    connectionLimit: 20,
+    host: "localhost",
+    user: "root",
+    password: "Mur@ngi02",
+    database: "test",
+    
+  })
+
+
+// Get all books
+app.get("/", (req, res) => {
+    const sql = "SELECT * FROM test";
+    db.query(sql, (err, result) => {
+      if (err) return res.json ({Message :"Error inside server"});
+        return res.json(result);
+      })
+      
+    })
+  
+
+
+
 //initializes an Express server to listen on port 8081
 app.listen (8081,()=>{console.log("listening");
 })

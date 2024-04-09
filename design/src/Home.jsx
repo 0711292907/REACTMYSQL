ad65efa1a566dc1c99ef91d544cc69c028f2cbc4
Jason <murangira99@gmail.com>
2024-04-09 10:33:04 +0200

    chnangef a few staff

--- design/src/Home.jsx
+++ design/src/Home.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 function Home() {
   useEffect(() => {
-    axios.get('http://localhost:8081/')
+    axios.get('http://localhost:8800/')
       .then(res => console.log(res))
       .catch(err => console.log(err));
   }, []); // Empty array means no dependencies
